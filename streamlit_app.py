f9cd3e57b9d91bf18c225bd8b7e22532e1f41510
frozenrage-omers <rguerzo@omers.com>
2025-08-28 08:50:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
@@ -46,7 +46,7 @@ if ingredients_list:
             sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
             #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
